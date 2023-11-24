32786ee9fd606492c4b0b804ff9b13c18fedec2f
Yoichi Ojima <yoichiojima@gmail.com>
2023-11-24 22:53:09 +0900

    update

--- japan-stats/common_process.py
+++ japan-stats/common_process.py
@@ -6,3 +6,6 @@ def cleanup_year(year):
     if _match:
         return _match.group()
 
+
+def strip_prefix(metrix_title):
+    return re.sub(r"\w\d*_", "", metrix_title)
--- japan-stats/economics/download.py
+++ japan-stats/economics/download.py
@@ -11,17 +11,14 @@ from common_process import cleanup_year, strip_prefix
 
 
 def main():
-    df = fetch_api.main("0000010102")
-
-    df["feature"] = df["Ｂ　自然環境"].apply(strip_prefix)
+    df = fetch_api.main("0000010103")
     df["year"] = df["調査年"].apply(cleanup_year)
-    df["unit"] = df["@unit"].apply(lambda x: unicodedata.normalize("NFKC", x))
-
-    df = df.rename(columns = {"地域": "area", "$": "value"})
+    df["feature"] = df["Ｃ　経済基盤"].apply(strip_prefix)
+    df = df.rename(columns = {"地域": "area", "@unit": "unit", "$": "value"})
     df = df[["feature", "year", "area", "unit", "value"]]
 
-    df.to_csv(Path(os.getenv("APPROOT")) / "data/environment.csv", index = False)
-    print("saved environment.csv")
+    df.to_csv(Path(os.getenv("APPROOT")) / "data/economics.csv", index = False)
+    print("saved economics.csv")
 
 
 if __name__ == "__main__":
