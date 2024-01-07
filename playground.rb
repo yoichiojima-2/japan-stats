78cdb7b9eab50b92055aac04517d62a0b62264eb
Yoichi Ojima <yoichiojima@gmail.com>
2024-01-07 20:37:30 +0900

    investigate api schema

--- playground.rb
+++ playground.rb
@@ -26,18 +26,19 @@ def get_data(id)
   base_url = "https://api.e-stat.go.jp/rest/#{ENV['API_VERSION']}/app/json/getStatsData"
   params = {
     appId: ENV['APP_ID'],
-    statsDataId: id
+    statsDataId: id,
   }
   uri = URI(base_url)
   uri.query = URI.encode_www_form(params)
   Net::HTTP.get_response(uri)
 end
 
-def main(stats_id_list, target)
-  res = get_data(stats_id_list[target])
-  data = JSON.parse(res.body)
-  data = data['GET_STATS_DATA']['STATISTICAL_DATA']['DATA_INF']['VALUE']
-  puts data
+def get_keys(response)
+  data = JSON.parse(response.body)
+  puts data["GET_STATS_DATA"]["STATISTICAL_DATA"]["DATA_INF"]["VALUE"]
 end
 
-main(stats_id_list, :population)
+
+res = get_data(stats_id_list[:population])
+puts get_keys(res)
+
